501a9939836cf47857bde1ef20af75e15b3e36b5
nagcheff <nagcheff@gmail.com>
2019-04-19 12:38:55 +0530

    toomuch

---
+++ login.java
@@ -0,0 +1,10 @@
+class Login
+{
+        public static void main(String args[])
+	        {
+		                 System.out.println("this is my worst login java program");
+
+
+				          }
+					           }
+						   
---
+++ payment.java
@@ -0,0 +1,10 @@
+class Payment
+{
+        public static void main(String args[])
+	        {
+		                 System.out.println("this is my worst payment java program");
+
+
+				          }
+					           }
+
